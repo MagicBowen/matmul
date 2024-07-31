2f43c5ddfeb87c5fd0a136be953eda6112b8f015
magicbowen <e.bowen.wang@icloud.com>
2024-07-31 09:19:34 +0800

    modify auto to decltype(auto)

--- include/matmul_keyword.h
+++ include/matmul_keyword.h
@@ -34,14 +34,14 @@ typename MATMUL_IMPL__::template MatmulDfxProxy<const typename IMPL::NAME> {*MAT
 #define MATMUL_MODULE(NAME)      cast_to_##NAME()
 
 #define MATMUL_USE_MODULE(NAME)                                 \
-inline constexpr auto MATMUL_MODULE(NAME) {                     \
+inline constexpr decltype(auto) MATMUL_MODULE(NAME) {           \
     if constexpr (MatmulDfxCfg::ENABLE) {                       \
         return MATMUL_CAST_TO_PROXY_OF(NAME);                   \
     } else {                                                    \
         return MATMUL_CAST_TO_IMPL_OF(NAME);                    \
     }                                                           \
 }                                                               \
-inline constexpr auto MATMUL_MODULE(NAME) const {               \
+inline constexpr decltype(auto) MATMUL_MODULE(NAME) const {     \
     if constexpr (MatmulDfxCfg::ENABLE) {                       \
         return MATMUL_CAST_TO_CONST_PROXY_OF(NAME);             \
     } else {                                                    \
@@ -50,14 +50,14 @@ inline constexpr auto MATMUL_MODULE(NAME) const {               \
 }
 
 #define MATMUL_USE_MODULE_ON(NAME, ...)                         \
-inline constexpr auto MATMUL_MODULE(NAME) {                     \
+inline constexpr decltype(auto) MATMUL_MODULE(NAME) {           \
     if constexpr (MatmulDfxCfg::ENABLE) {                       \
         return MATMUL_CAST_TO_PROXY_OF(template NAME<__VA_ARGS__>);\
     } else {                                                    \
         return MATMUL_CAST_TO_IMPL_OF(template NAME<__VA_ARGS__>); \
     }                                                           \
 }                                                               \
-inline constexpr auto MATMUL_MODULE(NAME) const {               \
+inline constexpr decltype(auto) MATMUL_MODULE(NAME) const {     \
     if constexpr (MatmulDfxCfg::ENABLE) {                       \
         return MATMUL_CAST_TO_CONST_PROXY_OF(template NAME<__VA_ARGS__>);\
     } else {                                                    \
@@ -87,7 +87,7 @@ MATMUL_POLICY_TEMPLATE<MATMUL_IMPL_TYPE, A_TYPE, B_TYPE, C_TYPE, BIAS_TYPE, MM_C
 /////////////////////////////////////////////////////////////////
 #define MATMUL_PRIVATE_TEMPLATE    MatmulPrivateModules
 
-#define MATMUL_MODULE_IN_PRIVATE(...)                          \
+#define MATMUL_MODULE_IN_PRIVATE(...)                           \
 MATMUL_PRIVATE_TEMPLATE<MATMUL_IMPL_TYPE, A_TYPE, B_TYPE, C_TYPE, BIAS_TYPE, MM_CFG>::__VA_ARGS__
 
 #define MATMUL_IMPORT_MODULE_PRIVATE(...)  private MATMUL_MODULE_IN_PRIVATE(__VA_ARGS__)
@@ -102,7 +102,7 @@ using NAME = typename MATMUL_MODULE_IN_POLICY(template NAME<__VA_ARGS__>)
 
 /////////////////////////////////////////////////////////////////
 #define MATMUL_ALLOW_USING_PRIVATE(NAME)                        \
-friend typename MATMUL_MODULE_IN_PRIVATE(NAME);                \
+friend typename MATMUL_MODULE_IN_PRIVATE(NAME);                 \
 using NAME = typename MATMUL_MODULE_IN_PRIVATE(NAME)
 
 #define MATMUL_ALLOW_USING_TEMPLATE_PRIVATE(NAME, ...)          \
@@ -110,7 +110,7 @@ using NAME = typename MATMUL_MODULE_IN_PRIVATE(template NAME<__VA_ARGS__>)
 
 /////////////////////////////////////////////////////////////////
 #define MATMUL_CONTEXT()   MATMUL_CAST_TO_IMPL()->var
-#define MATMUL_CONTEXT_CONST() ((const MATMUL_IMPL__*)(this))->var
+#define MATMUL_CONTEXT_CONST() MATMUL_CAST_TO_CONST_IMPL()->var
 
 #define MATMUL_PARAM_VAR   MATMUL_CONTEXT()
 #define MATMUL_CONST_PARAM_VAR  MATMUL_CONTEXT_CONST()
